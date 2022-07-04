a8b33466b7b8db68e48aa0b1674883cd6ad66f65
Sexettin <72075007+sexettin78@users.noreply.github.com>
2022-07-04 04:39:36 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Net;
+
+namespace habervericekme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            listBox1.Items.Clear();
+            XmlTextReader xmloku = new XmlTextReader("http://hurriyet.com.tr/rss/anasayfa"); 
+            while (xmloku.Read()) 
+            {
+                if (xmloku.Name == "title")
+                {
+                    listBox1.Items.Add(xmloku.ReadString());
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            listBox1.Items.Clear();
+            XmlTextReader xmloku = new XmlTextReader("http://www.milliyet.com.tr/rss/rssNew/gundemRss.xml");
+            while (xmloku.Read())
+            {
+                if (xmloku.Name == "title")
+                {
+                    listBox1.Items.Add(xmloku.ReadString());
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            XmlTextReader xmloku = new XmlTextReader("http://www.fotomac.com.tr/rss/anasayfa.xml");
+            while (xmloku.Read())
+            {
+                if (xmloku.Name == "title")
+                {
+                    listBox1.Items.Add(xmloku.ReadString());
+                }
+            }
+        }
+    }
+}
